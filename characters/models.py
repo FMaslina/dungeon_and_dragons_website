6122a92b854c0601ff9746624902b81849e25900
FMaslina <FMaslina@yandex.ru>
2024-03-09 19:16:28 +0700

    Add Character model

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Character(models.Model):
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name="Владелец")
+    name = models.TextField(null=True, blank=True, verbose_name="Имя")
+    # race = models.ForeignKey(to=Race, on_delete=models.CASCADE, verbose_name="Раса")
+    avaliable_stat_points = models.IntegerField(null=True, blank=True, verbose_name="Доступные очки характеристик")
+    strength = models.IntegerField(null=True, blank=True, verbose_name="Сила")
+    dexterity = models.IntegerField(null=True, blank=True, verbose_name="Ловкость")
+    constitution = models.IntegerField(null=True, blank=True, verbose_name="Телосложение")
+    intelligence = models.IntegerField(null=True, blank=True, verbose_name="Интеллект")
+    wisdom = models.IntegerField(null=True, blank=True, verbose_name="Мудрость")
+    charisma = models.IntegerField(null=True, blank=True, verbose_name="Харизма")
+    health = models.IntegerField(null=True, blank=True, verbose_name="Макс. здоровье")
+    mana = models.IntegerField(null=True, blank=True, verbose_name="Макс. мана")
+    current_health = models.IntegerField(null=True, blank=True, verbose_name="Текущее здоровье")
+    current_mana = models.IntegerField(null=True, blank=True, verbose_name="Текущая мана")
+    armor_class = models.IntegerField(null=True, blank=True, verbose_name="Класс доспеха")
+
+    class Meta:
+        verbose_name = 'Персонаж'
+        verbose_name_plural = 'Персонажи'
