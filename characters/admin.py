cdf13e5616e7b046ff36482e05e9af58db830e0b
FMaslina <FMaslina@yandex.ru>
2024-03-09 19:41:53 +0700

    Add Skill, Race, Character models to admin

--- characters/admin.py
+++ characters/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Skill, Race, Character
 
-# Register your models here.
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'effect',)
+    search_fields = ('name', 'effect',)
+    list_filter = ('name',)
+
+
+admin.site.register(Skill, SkillAdmin)
+
+
+class RaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_character_stat_points',)
+    search_fields = ('name',)
+    list_filter = ('start_character_stat_points',)
+
+
+admin.site.register(Race, RaceAdmin)
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'name', 'race', 'avaliable_stat_points', 'health', 'mana', 'armor_class',)
+    search_fields = ('owner', 'name', 'race',)
+    list_filter = ('owner', 'race',)
+
+
+admin.site.register(Character, CharacterAdmin)
