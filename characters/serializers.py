d9cab8a903c278f97378450aac61ae2d69b0548f
FMaslina <FMaslina@yandex.ru>
2024-04-14 13:43:57 +0700

    Create RaceSerializer class

--- characters/serializers.py
+++ characters/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from dungeon_and_dragons.characters.models import Skill
+from dungeon_and_dragons.characters.models import Skill, Race
 
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
+        fields = "__all__"
+
+
+class RaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Race
         fields = "__all__"
