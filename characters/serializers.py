959ef17f68bf9acb093109f58089b1d44bf299da
FMaslina <FMaslina@yandex.ru>
2024-04-14 13:43:10 +0700

    Create SkillSerializer class

--- characters/serializers.py
+++ characters/serializers.py
@@ -1,2 +1,8 @@
 from rest_framework import serializers
+from dungeon_and_dragons.characters.models import Skill
 
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = "__all__"
